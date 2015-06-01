8f357fb6bff0f0c1ccb9d619e009752fcdb48909
Emil Persson <emil.n.persson@gmail.com>
2015-06-01 20:21:06 +0200

    Cleanup

--- lib/src/database/library.dart
+++ lib/src/database/library.dart
@@ -1,21 +1,17 @@
 library bridge.database;
 
 // Core libraries
-import 'dart:async';
-import 'dart:mirrors';
+//import 'dart:async';
+//import 'dart:mirrors';
 
 // Server side
-import 'package:bridge/bridge.dart';
+//import 'package:bridge/bridge.dart';
 import 'package:bridge/core.dart';
 
-// Implementing cupid output
-//import 'package:cupid/print.dart';
-export 'package:cupid/print.dart';
-
 // External libraries
-import 'package:objectory/objectory.dart';
-import 'package:objectory/objectory_console.dart';
+//import 'package:objectory/objectory.dart';
+//import 'package:objectory/objectory_console.dart';
 
 part 'database_service_provider.dart';
-part 'model.dart';
-part 'repository.dart';
+
+const field = 'field';
