e57f08a122d1842c8318a5f7f76d403e31b595c8
Emil Persson <emil.n.persson@gmail.com>
2015-06-03 17:11:31 +0200

    TetherManager can broadcast messages

--- lib/src/tether/server/tether_manager.dart
+++ lib/src/tether/server/tether_manager.dart
@@ -8,10 +8,13 @@ abstract class TetherManager {
   void registerHandler(TetherHandler handler);
 
   void manage(Tether tether);
+
+  void broadcast(String key, [data]);
 }
 
 class _TetherManager implements TetherManager {
   final List<TetherHandler> _handlers = [];
+  final List<Tether> _tethers = [];
 
   void registerHandler(TetherHandler handler) {
     _handlers.add(handler);
@@ -19,9 +22,15 @@ class _TetherManager implements TetherManager {
 
   void manage(Tether tether) {
     _passTetherThroughHandlers(tether);
+    _tethers.add(tether);
+    tether.onConnectionLost.then((_) => _tethers.remove(tether));
   }
 
   void _passTetherThroughHandlers(Tether tether) {
     _handlers.forEach((h) => h(tether));
+  }
+
+  void broadcast(String key, [data]) {
+    _tethers.forEach((t) => t.send(key, data));
   }
 }
