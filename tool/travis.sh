03f8b9eae78087be668fb6ec54705d20a3eb8d85
Emil Persson <emil.n.persson@gmail.com>
2015-06-03 10:33:38 +0200

    Travis script

---
+++ tool/travis.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Fast fail the script on failures.
+set -e
+
+dartanalyzer bin/dart_coveralls.dart lib/dart_coveralls.dart
+
+pub run test test/
+
+# Install dart_coveralls; gather and send coverage data.
+if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
+  echo "Running coverage..."
+  pub run dart_coveralls report \
+    --retry 2 \
+    --exclude-test-files \
+    --debug \
+    test/all.dart
+  echo "Coverage complete."
+else
+  if [ -z ${COVERALLS_TOKEN+x} ]; then echo "COVERALLS_TOKEN is unset"; fi
+  if [ -z ${TRAVIS_DART_VERSION+x} ]; then
+    echo "TRAVIS_DART_VERSION is unset";
+  else
+    echo "TRAVIS_DART_VERSION is $TRAVIS_DART_VERSION";
+  fi
+
+  echo "Skipping coverage for this configuration."
+fi
