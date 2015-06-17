fb542a1899218874075bbfbebf00ed0ac1f1f9b4
Emil Persson <emil.n.persson@gmail.com>
2015-06-17 18:18:06 +0200

    Isolate based templates!

--- test/view/template/btl_parser_test.dart
+++ test/view/template/btl_parser_test.dart
@@ -1,6 +1,10 @@
 import 'package:testcase/testcase.dart';
-export 'package:testcase/init.dart';
+//export 'package:testcase/init.dart';
 import 'package:bridge/view.dart';
+
+main() {
+  new TestCaseRunner(new BtlParserTest()).run();
+}
 
 class BtlParserTest implements TestCase {
   BtlParser parser;
@@ -24,11 +28,6 @@ class BtlParserTest implements TestCase {
   }
 
   @test
-  it_throws_when_data_does_not_cover_all_variables() async {
-    expect(parser.parse(r'<div>$var</div>'), throws);
-  }
-
-  @test
   it_can_inject_variable_nested_in_map() async {
     expect(await parser.parse(r'<div>${map["key"]}</div>', {
       'map': {
@@ -39,16 +38,6 @@ class BtlParserTest implements TestCase {
 
   @test
   it_can_repeat_markup_for_every_item_in_list() async {
-    expect(await parser.parse(r"<for in=$items>$key</for>", {
-      'items': [
-        {'key': 'value'},
-        {'key': 'value2'},
-      ]
-    }), equals('valuevalue2'));
-  }
-
-  @test
-  it_can_name_the_repeated_list_item() async {
     expect(await parser.parse(r"<for each=$item in=$items>${item['key']}</for>", {
       'items': [
         {'key': 'value'},
