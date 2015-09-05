6fe44dec96033fd6e37e5924953852ddc688fb94
Emil Persson <emil.n.persson@gmail.com>
2015-09-06 01:27:15 +0200

    Expose more Trestle API

--- lib/src/database/library.dart
+++ lib/src/database/library.dart
@@ -5,5 +5,7 @@ import 'dart:async';
 import 'package:bridge/core.dart';
 
 import 'package:trestle/gateway.dart';
+export 'package:trestle/gateway.dart';
+export 'package:trestle/trestle.dart';
 
 part 'database_service_provider.dart';
