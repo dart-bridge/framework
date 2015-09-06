e721f9a1e5cdcf06041c0fe35ea4575c8c16e3fb
Emil Persson <emil.n.persson@gmail.com>
2015-09-06 02:15:50 +0200

    Repository connection workaround

--- lib/src/database/library.dart
+++ lib/src/database/library.dart
@@ -5,7 +5,8 @@ import 'dart:async';
 import 'package:bridge/core.dart';
 
 import 'package:trestle/gateway.dart';
+import 'package:trestle/trestle.dart' as trestle;
 export 'package:trestle/gateway.dart';
-export 'package:trestle/trestle.dart';
+export 'package:trestle/trestle.dart' hide Repository;
 
 part 'database_service_provider.dart';
