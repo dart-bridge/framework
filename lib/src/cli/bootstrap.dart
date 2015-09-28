32018c4eb99968fec785fb3c864d67f7f1dc1900
Emil Persson <emil.n.persson@gmail.com>
2015-09-28 23:39:15 +0200

    Fix a comment

--- lib/src/cli/bootstrap.dart
+++ lib/src/cli/bootstrap.dart
@@ -1,7 +1,7 @@
 part of bridge.cli;
 
-/// Entry point for the server side application. The `arguments` and `message`
-/// arguments should be passed directly from the main function. The `configPath`
+/// Entry point for the server side application. The `args` argument
+/// should be passed directly from the main function. The `configPath`
 /// is the relative path to the root config directory. This will be passed
 /// into the [Application] and bootstrap the entire system.
 bootstrap(List<String> args, {String configPath}) async {
