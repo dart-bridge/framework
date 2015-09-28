fcc5e701aaa99b46ee043453167f255d21667bbf
Emil Persson <emil.n.persson@gmail.com>
2015-09-28 14:57:06 +0200

    Updates to Cupid cleans up bridge.cli

--- lib/src/cli/bootstrap.dart
+++ lib/src/cli/bootstrap.dart
@@ -5,18 +5,14 @@ part of bridge.cli;
 /// is the relative path to the root config directory. This will be passed
 /// into the [Application] and bootstrap the entire system.
 bootstrap(List<String> args, {String configPath}) async {
-  var arguments = args.toList();
+  final List arguments = args?.toList() ?? [];
+  var shell = new Shell();
   if (arguments.contains('--production')) {
-    _printToLog = true;
+    shell = new Shell(null, new FileOutputDevice());
     arguments.remove('--production');
   }
-  await _makeProgram(configPath, arguments).run(arguments);
-}
-
-bool _printToLog = false;
-
-Program _makeProgram(configPath, arguments) {
-  return new BridgeCli(arguments, _chooseConfigPath(configPath), printToLog: _printToLog);
+  return new BridgeCli(arguments, _chooseConfigPath(configPath), shell)
+      .run(arguments.join(' '));
 }
 
 String _chooseConfigPath(String configPath) {
