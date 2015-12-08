c27f5bdacc752e124891f25ebb8619e3d38cfc5a
Emil Persson <emil.n.persson@gmail.com>
2015-12-08 11:57:33 +0100

    Use status code from template in TemplatesMiddleware

--- lib/src/view/templates_middleware.dart
+++ lib/src/view/templates_middleware.dart
@@ -3,7 +3,16 @@ part of bridge.view;
 class TemplatesMiddleware extends Middleware {
   Future<Response> handle(Request request) {
     return super.handle(convert(request, Template, (Template template) {
-      return template.content.join('\n');
+      final code = template.data['errorCode']
+          ?? template.data['statusCode']
+          ?? template.data['code']
+          ?? template.data['status']
+          ?? 200;
+      return new Response(
+          code is int ? code : 200,
+          body: template.encoded, headers: {
+        'Content-Type': ContentType.HTML.toString()
+      });
     }));
   }
 }
