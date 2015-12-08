0fdd4f1c7d008f671c50095e248ae7768ba2b0c9
Emil Persson <emil.n.persson@gmail.com>
2015-12-08 16:19:59 +0100

    Include a way to use the full application when testing

--- lib/src/test/test_application.dart
+++ lib/src/test/test_application.dart
@@ -9,9 +9,6 @@ class TestApplication extends Application {
   TestApplication._(List<Type> serviceProviders, {Map config})
       : _config = new Config(config ?? {}),
         super() {
-    Environment.current = Environment.testing;
-    _program = new BridgeCli(null, new Shell(_io, _io));
-
     final Iterable<String> serviceProviderQualifiedNames = serviceProviders
         .map(reflectType)
         .map((TypeMirror m) => m.qualifiedName)
@@ -21,11 +18,22 @@ class TestApplication extends Application {
     _config['app']['service_providers'] ??= [];
     _config['app']['service_providers'].addAll(serviceProviderQualifiedNames);
 
+    _init();
+  }
+
+  void _init() {
+    Environment.current = Environment.testing;
+    _program = new BridgeCli(null, new Shell(_io, _io));
+
     singleton(_program);
     singleton(_program, as: Program);
     singleton(this);
     singleton(this, as: Application);
     singleton(this, as: Container);
+  }
+
+  TestApplication._full(this._config) : super() {
+    _init();
   }
 
   List<String> get log => _io.log;
@@ -45,6 +53,14 @@ class TestApplication extends Application {
   static Future<TestApplication> start(List<Type> serviceProviders,
       {Map config}) async {
     final app = new TestApplication._(serviceProviders, config: config);
+    await app.setUp();
+    return app;
+  }
+
+  static Future<TestApplication> startFull(
+      {String configDirectory: 'config'}) async {
+    final config = await Config.load(new Directory(configDirectory));
+    final app = new TestApplication._full(config);
     await app.setUp();
     return app;
   }
--- lib/test.dart
+++ lib/test.dart
@@ -7,6 +7,7 @@ import 'cli.dart';
 import 'core.dart';
 import 'package:bridge/http.dart' as http;
 import 'dart:convert';
+import 'dart:io';
 
 part 'src/test/input_output.dart';
 part 'src/test/test_application.dart';
